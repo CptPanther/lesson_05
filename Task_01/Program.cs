bcd8783e73d454081a13ef5d9d3e496637ad8032
Denis Rozhkov <al1ennnnn@yandex.ru>
2022-11-28 06:05:00 +0400

    'done'

---
+++ Task_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        WriteLine("Введите предложение:");
+        WriteList(GetList(ReadLine()));
+        ReadKey(true);
+    }
+
+    //Создает список
+    static string[] GetList(string text)
+    {
+        return text.Split(' ');
+    }
+
+    //Выводит список
+    static void WriteList(string[] list)
+    {
+        WriteLine("\nСписок слов: ");
+        foreach (string item in list)
+            WriteLine(item);
+    }
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        WriteLine("Введите предложение:");
+        WriteLine(InvertText(ReadLine()));
+        ReadKey(true);
+    }
+
+    //Создает список
+    static string[] GetList(string text)
+    {
+        return text.Split(' ');
+    }
+
+    //Инвертирует текст
+    static string InvertText(string text)
+    {
+        WriteLine("\nИнвертированный текст:");
+        string[] list = GetList(text);
+        string invText = "";
+
+        for (int item=list.Length-1; item>=0; item--)
+            invText += list[item]+" ";
+
+        return invText;
+    }
+}
